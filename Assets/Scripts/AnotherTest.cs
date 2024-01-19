a5ff7f36be693893288b38a1aeaf5bf0e46b9bd6
Jayden Quallio <52089788+JaydenQuallio@users.noreply.github.com>
2024-01-19 09:51:56 -0700

    Cycling terrain textures
    Removed old 111 colors
    Added Terrain Pref to timechange scene
    Linked slider on value changed to AnotherTest.UpdateTexture

--- Assets/Scripts/AnotherTest.cs
+++ Assets/Scripts/AnotherTest.cs
@@ -1,40 +1,35 @@
+using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AnotherTest : MonoBehaviour
 {
-    [SerializeField] Material material, newMat;
 
     [SerializeField] List<Texture2D> textures = new();
 
     [SerializeField] Renderer render;
 
-    [SerializeField]
-    int index = 0;
-    [SerializeField]
-    float timeToSwap = 1f, currTime = 0f;
+    Material newMat;
 
     private void Start()
     {
-        newMat = new Material(material);
-        newMat.mainTexture = textures[index];
+        // Creates a new material to edit without editing the og
+        newMat = new Material(render.material);
+        render.material = newMat;
+
+        // Sets the secondary albedo to the first texture in the index
+        render.material.EnableKeyword("_DetailAlbedoMap");
+        render.material.SetTexture("_DetailAlbedoMap", textures[0]);
+    }
+
+    // Updates texture based on passthrough of index assaigned to a slider change value
+    public void UpdateTexture(Single index)
+    {
+        int temp = index.ConvertTo<Int32>();
+
+        newMat.SetTexture("_DetailAlbedoMap", textures[temp - 1]);
         render.material = newMat;
     }
 
-    private void FixedUpdate()
-    {
-        currTime += Time.deltaTime;
-
-        if (currTime > timeToSwap)
-        {
-            currTime = 0f;
-            index++;
-
-            if(index > textures.Count - 1)
-                index = 0;
-
-            newMat.mainTexture = textures[index];
-            render.material = newMat;
-        }
-    }
 }
