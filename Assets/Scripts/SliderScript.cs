889de9de18167ebcbb7bce4de76b6c0455932e54
Mitchellol <mitchelloliarny@gmail.com>
2024-01-19 13:51:44 -0700

    Changed Date Format and Added Color array

--- Assets/Scripts/SliderScript.cs
+++ Assets/Scripts/SliderScript.cs
@@ -12,14 +12,15 @@ public class SliderScript : MonoBehaviour
     private Slider slider;
     [SerializeField] private Image sliderFill;
 
-    private string[] dates = { "2021-03-17","2021-03-27","2021-04-12","2021-04-23","2021-05-11","2021-05-26","2021-06-08","2021-06-23","2021-07-23","2021-08-10","2021-09-03","2021-09-30" };
-    private Color32[] colors = { new Color32(100, 20, 20, 255), new Color32(100, 50, 20, 255), new Color32(10, 100, 20, 255), new Color32(10, 150, 20, 255), new Color32(10, 255, 20, 255), new Color32(10, 150, 20, 255), new Color32(10, 100, 20, 255), new Color32(100, 50, 20, 255), new Color32(100, 20, 20, 255) };
+    private string[] dates = { "March 17, 2021","March 27, 2021","April 12, 2021","April 23, 2021","May 11, 2021","May 26, 2021","June 8, 2021","June 23, 2021","July 23, 2021","August 10, 2021","September 3, 2021","September 30, 2021" };
+    [SerializeField] private Color32[] colors;
 
     void Start()
     {
         slider = gameObject.GetComponent<Slider>();
         slider.maxValue = dates.Length;
         monthText.text = dates[0];
+        sliderFill.color = colors[0];
         slider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
     }
 
